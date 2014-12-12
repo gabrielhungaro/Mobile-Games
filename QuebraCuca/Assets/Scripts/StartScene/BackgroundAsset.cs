013c48d00c2fe4852665ee99bd2d4d362bc3a32f
Douglas Mendes Barreto <mendes-barreto@live.com>
2014-12-12 10:03:15 -0200

    New modifications to debuging better

---
+++ QuebraCuca/Assets/Scripts/StartScene/BackgroundAsset.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using AquelaFrameWork.Core.Asset;
+
+public class BackgroundAsset : MonoBehaviour 
+{
+#if UNITY_EDITOR
+
+
+
+#endif //UNITY_EDITOR
+
+    // Use this for initialization
+	void Start () 
+    {
+
+#if UNITY_EDITOR
+        AFAssetManager.SimulatedDPI = AFAssetManager.DPI_IPAD_3;
+        AFAssetManager.SimulatePlatform = AFAssetManager.EPlataform.IOS;
+        AFDebug.Log(AFAssetManager.GetPathTargetPlatformWithResolution());
+#endif //UNITY_EDITOR
+        
+        Sprite sp = AFAssetManager.Instance.Load<Sprite>(AFAssetManager.GetPathTargetPlatformWithResolution() + "/" + "Scenes/Login/Background");
+        Image L_img =  gameObject.GetComponent<Image>();
+        L_img.sprite = sp;
+    }
+	
+}
---
+++ QuebraCuca/Assets/Scripts/StartScene/StartSceneMain.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using AquelaFrameWork.Core.Asset;
+using AquelaFrameWork.Core;
+using AquelaFrameWork.Core.State;
+
+
+public class StartSceneMain : AFEngine 
+{
+
+    public override void Initialize()
+    {
+        AFDebug.SetConfigs( AFDebugSettings.OUTPUT_SCREEN | AFDebugSettings.OUTPUT_UNITY );
+
+        m_stateManager = AFObject.Create<AFStateManager>();
+        m_stateManager.Initialize(new StartStateFactory());
+        m_stateManager.GotoState(AState.EGameState.MENU);
+
+//         _resolutionController = ResolutionController.Instance();
+//         _paths = PathConstants.Instance();
+//         _sounds = SoundConstants.Instance();
+// 
+//         this.gameObject.AddComponent<SoundManager>().Init();
+//         //SoundManager.PlaySoundByName(SoundConstants.BG_SOUND);
+// 
+//         base.Initialize();
+// 
+        base.Initialize();
+
+
+        AFSingleTransition tras = AFObject.Create<AFSingleTransition>();
+        SpriteRenderer sr = tras.gameObject.AddComponent<SpriteRenderer>();
+        sr.sprite = AFAssetManager.Instance.CreateSpriteFromTexture("Common/High/loadingscreen");
+        sr.transform.localScale = new Vector3(1, 1, 1);
+        m_stateManager.AddTransition(tras);
+    }
+}
